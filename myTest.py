5fbd7da9ab3829c1a12655b98f68e148fa499aca
glhr <grenblot@gmail.com>
2020-09-18 00:09:54 +0200

    updating test script with verbose output to view predictions

--- myTest.py
+++ myTest.py
@@ -7,7 +7,7 @@ python3 myTest.py -ds= -path= -bsz=
 
 directory = args['path'].split("/")
 HDD = directory[2].split('HDD')[1].split('BSZ')[0]
-decoder = directory[1].split('-')[0] 
+decoder = directory[1].split('-')[0]
 BSZ = int(args['batch']) if args['batch'] else int(directory[2].split('BSZ')[1].split('DR')[0])
 args["decoder"] = decoder
 args["HDD"] = HDD
@@ -20,26 +20,29 @@ else:
 
 train, dev, test, test_special, lang, SLOTS_LIST, gating_dict, max_word = prepare_data_seq(False, args['task'], False, batch_size=BSZ)
 
+print(SLOTS_LIST)
+print(gating_dict)
+print(max_word)
+print(test)
+
 model = globals()[decoder](
-    int(HDD), 
-    lang=lang, 
-    path=args['path'], 
-    task=args["task"], 
-    lr=0, 
+    int(HDD),
+    lang=lang,
+    path=args['path'],
+    task=args["task"],
+    lr=0,
     dropout=0,
     slots=SLOTS_LIST,
     gating_dict=gating_dict,
     nb_train_vocab=max_word)
 
-if args["run_dev_testing"]:
-    print("Development Set ...")
-    acc_dev = model.evaluate(dev, 1e7, SLOTS_LIST[2]) 
+# if args["run_dev_testing"]:
+#     print("Development Set ...")
+#     acc_dev = model.evaluate(dev, 1e7, SLOTS_LIST[2])
+#
+# if args['except_domain']!="" and args["run_except_4d"]:
+#     print("Test Set on 4 domains...")
+#     acc_test_4d = model.evaluate(test_special, 1e7, SLOTS_LIST[2])
 
-if args['except_domain']!="" and args["run_except_4d"]:
-    print("Test Set on 4 domains...")
-    acc_test_4d = model.evaluate(test_special, 1e7, SLOTS_LIST[2]) 
-
-print("Test Set ...")
-acc_test = model.evaluate(test, 1e7, SLOTS_LIST[3]) 
-
-
+# print("Test Set ...")
+acc_test = model.predict(test, 1e7, SLOTS_LIST[3], verbose=True, max_samples=0)
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python myTest.py --path=save/TRADE-multiwozdst/HDD400BSZ32DR0.2ACC-0.4875/
